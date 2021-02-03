60bd8dc6e1fc520e3593f4b87fb1880b63ead12c
Hadrien Kohl <hadrien.kohl@gmail.com>
2021-02-03 18:36:12 +0100

    Implement the storage service

--- src/main/java/no/ssb/dapla/datamaintenance/storage/StorageService.java
+++ src/main/java/no/ssb/dapla/datamaintenance/storage/StorageService.java
@@ -1,20 +1,85 @@
 package no.ssb.dapla.datamaintenance.storage;
 
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.storage.StorageOptions;
+import com.google.cloud.storage.contrib.nio.CloudStorageConfiguration;
+import com.google.cloud.storage.contrib.nio.CloudStorageFileSystem;
 import io.helidon.common.reactive.Multi;
 import io.helidon.common.reactive.Single;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ForkJoinPool;
+import java.util.function.BiPredicate;
 
 /**
  * Two phase delete on object store.
  */
 public class StorageService {
 
+    private static final String GCS_SCHEME = "gs";
+    private static final String FILE_SCHEME = "file";
+    private static final String DELETED_MARKER = ".DELETED";
+
+    private Executor executor = null;
+
+    // We might need to cache the file system by bucket with token expiration as eviction?
+    StorageOptions getStorageOptions(InputStream token) throws IOException {
+        return StorageOptions.newBuilder()
+                .setCredentials(ServiceAccountCredentials.fromStream(token))
+                .build();
+    }
+
+    FileSystem setupFileSystem(URI prefix, InputStream token) throws IOException {
+        var scheme = prefix.getScheme();
+        if (GCS_SCHEME.equalsIgnoreCase(scheme)) {
+            var bucketName = prefix.getHost();
+            return CloudStorageFileSystem.forBucket(bucketName, CloudStorageConfiguration.DEFAULT, getStorageOptions(token));
+        } else if (FILE_SCHEME.equalsIgnoreCase(scheme)) {
+            return FileSystems.getDefault();
+        } else {
+            throw new UnsupportedOperationException("scheme " + scheme + " is not supported");
+        }
+    }
+
+    static URI removeSchemeAndHost(URI path) {
+        var scheme = path.getScheme();
+        if (GCS_SCHEME.equalsIgnoreCase(scheme)) {
+            // Strip the scheme and bucket name for GCS.
+            return URI.create(path.getPath());
+        } else {
+            return path;
+        }
+    }
+
+    Executor getExecutor() {
+        return executor != null ? executor : ForkJoinPool.commonPool();
+    }
+
     /**
      * Mark the prefix to be deleted
      *
      * @param prefix the prefix to delete.
      */
-    public Single<String> markDelete(String prefix) {
-        return Single.empty();
+    public Single<Path> markDelete(URI prefix, InputStream token) throws IOException {
+        var fileSystem = setupFileSystem(prefix, token);
+        var file = removeSchemeAndHost(prefix);
+        var deleteMarker = fileSystem.getPath(file.getPath(), DELETED_MARKER);
+        try {
+            Files.createFile(deleteMarker);
+        } catch (FileAlreadyExistsException faee) {
+            // TODO: Log
+        }
+        return Single.just(deleteMarker);
     }
 
     /**
@@ -23,8 +88,60 @@ public class StorageService {
      * @param prefix the prefix
      * @return the list of files that are deleted.
      */
-    public Multi<String> finishDelete(String prefix) {
-        return Multi.empty();
+    public Multi<Path> finishDelete(URI prefix, InputStream token) throws IOException {
+        var fileSystem = setupFileSystem(prefix, token);
+        var path = fileSystem.getPath(removeSchemeAndHost(prefix).getPath());
+
+        // TODO: check if a recursive implementation with Files.find() and a folder
+        //  filter would improve performances. I suspect that GCS under the hood need
+        //  to go through the files sequentially anyways.
+        return Multi.concat(
+                Multi.just(path),
+                Multi.create(Files.list(path))
+                ).filter(subDirectory -> {
+                    return Files.isDirectory(subDirectory) && Files.exists(subDirectory.resolve(DELETED_MARKER));
+                }).flatMap(pathToDelete -> deleteAllUnder(pathToDelete));
     }
 
+    /**
+     * Delete the files marked for deletion
+     *
+     * @param prefix the prefix
+     * @return the list of files that are deleted.
+     */
+    private Multi<Path> deleteAllUnder(Path prefix) {
+        return findAsync(prefix, Integer.MAX_VALUE, (foundPath, attr) -> !attr.isDirectory())
+                .flatMap(pathToDelete -> deleteIfExistsAsync(pathToDelete));
+    }
+
+    /**
+     * Like {@link Files#find} but async.
+     */
+    private Multi<Path> findAsync(Path start, int maxDepth, BiPredicate<Path, BasicFileAttributes> matcher,
+                                  FileVisitOption... options) {
+        return Single.defer(() -> {
+            try {
+                return Single.just(Files.find(start, maxDepth, (foundPath, attr) -> !attr.isDirectory()));
+            } catch (IOException ioe) {
+                return Single.error(ioe);
+            }
+        }).observeOn(getExecutor()).flatMap(Multi::create);
+    }
+
+    /**
+     * Like {@link Files#deleteIfExists(Path)} but async.
+     */
+    private Single<Path> deleteIfExistsAsync(Path path) {
+        return Single.defer(() -> {
+            try {
+                if (Files.deleteIfExists(path)) {
+                    return Single.just(path);
+                } else {
+                    return Single.empty();
+                }
+            } catch (IOException ioe) {
+                return Single.error(ioe);
+            }
+        }).observeOn(getExecutor());
+    }
 }
---
+++ src/test/java/no/ssb/dapla/datamaintenance/storage/StorageServiceTest.java
@@ -0,0 +1,89 @@
+package no.ssb.dapla.datamaintenance.storage;
+
+import com.google.cloud.storage.StorageOptions;
+import com.google.cloud.storage.contrib.nio.CloudStorageConfiguration;
+import com.google.cloud.storage.contrib.nio.CloudStorageFileSystem;
+import com.google.cloud.storage.contrib.nio.testing.LocalStorageHelper;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.nio.file.FileSystem;
+import java.nio.file.Files;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StorageServiceTest {
+
+    @Test
+    void testThatSchemeAndBucketAreRemoved() {
+        var gsPath = StorageService.removeSchemeAndHost(URI.create("gs://bucket-name/foo/bar"));
+        assertThat(gsPath.toASCIIString()).isEqualTo("/foo/bar");
+
+        var filePath = StorageService.removeSchemeAndHost(URI.create("file:///foo/bar"));
+        assertThat(filePath.toASCIIString()).isEqualTo("file:///foo/bar");
+    }
+
+    @Test
+    void testDeleteWorks() throws IOException {
+        var service = new TestableStorageService();
+        var fs = service.getFileSystem("fake-bucket-1");
+
+        // Setup some files.
+        Files.createDirectories(fs.getPath("/foo/bar"));
+        Files.createFile(fs.getPath("/foo/bar/file1"));
+        Files.createFile(fs.getPath("/foo/bar/file2"));
+
+        Files.createDirectories(fs.getPath("/bar/foo"));
+        Files.createFile(fs.getPath("/bar/foo/file1"));
+        Files.createFile(fs.getPath("/bar/foo/file2"));
+
+        Files.createDirectories(fs.getPath("/foo/bar/baz"));
+        Files.createFile(fs.getPath("/foo/bar/baz/file1"));
+        Files.createFile(fs.getPath("/foo/bar/baz/file2"));
+
+        var deleteMarker = service.markDelete(URI.create("gs://fake-bucket-1/foo/bar"), InputStream.nullInputStream())
+                .await();
+
+        assertThat(deleteMarker).asString().isEqualTo("/foo/bar/.DELETED");
+
+        assertThat(Files.exists(fs.getPath("/foo/bar/file1"))).isTrue();
+        assertThat(Files.exists(fs.getPath("/foo/bar/file2"))).isTrue();
+
+        assertThat(Files.exists(fs.getPath("/bar/foo/file1"))).isTrue();
+        assertThat(Files.exists(fs.getPath("/bar/foo/file2"))).isTrue();
+
+        assertThat(Files.exists(fs.getPath("/foo/bar/baz/file1"))).isTrue();
+        assertThat(Files.exists(fs.getPath("/foo/bar/baz/file2"))).isTrue();
+
+        var deletedFiles = service.finishDelete(URI.create("gs://fake-bucket-1/foo/bar"), InputStream.nullInputStream())
+                .map(path -> path.toString()).collectList().await();
+
+        assertThat(deletedFiles).containsExactly(
+                "/foo/bar/file1",
+                "/foo/bar/file2",
+                "/foo/bar/.DELETED",
+                "/foo/bar/baz/file1",
+                "/foo/bar/baz/file2"
+        );
+    }
+
+    private static final class TestableStorageService extends StorageService {
+
+        private static final StorageOptions STORAGE_OPTIONS = LocalStorageHelper.getOptions();
+
+        @Override
+        StorageOptions getStorageOptions(InputStream token) {
+            return STORAGE_OPTIONS;
+        }
+
+        public FileSystem getFileSystem(String bucketName) {
+            return CloudStorageFileSystem.forBucket(
+                    bucketName,
+                    CloudStorageConfiguration.DEFAULT,
+                    getStorageOptions(null)
+            );
+        }
+    }
+}
